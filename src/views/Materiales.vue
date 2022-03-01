03fde136d9593a53b43d8b76c8b39010894b952e
JonathanDev <jonathan.cardozo.dev@gmail.com>
2022-03-01 17:13:46 -0500

    Add Component Footer

--- src/views/Materiales.vue
+++ src/views/Materiales.vue
@@ -1,14 +1,16 @@
 <template>
-    <div>
-
-    </div>
+  <div><app-header></app-header><app-footer></app-footer></div>
+</template>
 
 <script>
+import AppHeader from "@/components/AppHeader.vue";
+import AppFooter from "@/components/AppFooter.vue";
+
 export default {
-    
-}
+  name: "Materiales",
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+};
 </script>
-
-<style scoped>
-
-</style>
--- src/views/Muebles.vue
+++ src/views/Muebles.vue
@@ -1,28 +1,32 @@
 <template>
-    <div>
-        <app-header></app-header>
-        <users v-bind:users="users"></users>
-    </div>
+  <div>
+    <app-header></app-header>
+    <users v-bind:users="users"></users>
+    <app-footer></app-footer>
+  </div>
 </template>
 
 <script>
-import api from '@/api'
-import Users from '@/components/Users.vue'
-import AppHeader from '@/components/AppHeader'
+import api from "@/api";
+import Users from "@/components/Users.vue";
+import AppHeader from "@/components/AppHeader";
+import AppFooter from "@/components/AppFooter";
 
-export default ({
-    components:{
-        Users,
-        AppHeader
-    },
-    data(){
-        return{
-            users: []
-        } 
-    },
-    created(){      //hook created, justo cuando se crea el/este componente se ejecuta esta función
-        api.getUsersPromise().then(data => this.users = data)
-        //api.getUsersPromise().then(data => console.log(data))   //cuando llegue la info la imprima
-    }
-})
+export default {
+  components: {
+    Users,
+    AppHeader,
+    AppFooter,
+  },
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    //hook created, justo cuando se crea el/este componente se ejecuta esta función
+    api.getUsersPromise().then((data) => (this.users = data));
+    //api.getUsersPromise().then(data => console.log(data))   //cuando llegue la info la imprima
+  },
+};
 </script>
